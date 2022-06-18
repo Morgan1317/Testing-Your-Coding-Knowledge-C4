fb805de097823f2ad3e996000e82fb517550c97d
Morgan Holcomb <morganholcomb2019@gmail.com>
2022-06-18 14:29:22 -0500

    question displays and gets rid of start page and timer works'

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,31 @@
-var body = document.body;
-// var h1El = document.createElement('h1');
-// var listEl = document.createElement('ol');
-// var a1 = document.createElement('li');
-// var a2 = document.createElement('li');
-// var a3 = document.createElement('li');
-// var a4 = document.createElement('li');
-
-// var questionEl = document.createElement('div');
-
 var timerEl = document.getElementById('timer');
 var startButton = document.getElementById('begin');
 var instructions = document.getElementById('paragraph');
 var questionEl = document.getElementById('question');
+var title = document.getElementById('title');
+var list = document.getElementById('list')
 var a1 = document.getElementById('a1');
 var a2 = document.getElementById('a2');
 var a3 = document.getElementById('a3');
 var a4 = document.getElementById('a4');
 
+list.style.display='none';
 
 
-function timer(){
+
+
+
+
+function startQuiz(){
+    title.textContent ='';
+    instructions.textContent='';
+    questionEl.textContent = 'Commonly used data types DO not Include:'
+    a1.textContent = 'strings';
+    a2.textContent = 'answer 2';
+    a3.textContent = 'answer 3';
+    a4.textContent = 'answer 4';
+    list.style.display='inherit'
+    startButton.style.display = 'none';
     // starts with 120 seconds
     var time = 120;
 
@@ -34,10 +40,9 @@ function timer(){
         }
     },1000);
 };
-questionEl.textContent = 'Question 1';
-a1.textContent = 'answer 1';
-a2.textContent = 'answer 2';
-a3.textContent = 'answer 3';
-a4.textContent = 'answer 4';
 
-startButton.addEventListener('click', timer);
+
+
+
+startButton.addEventListener('click',startQuiz);
+
